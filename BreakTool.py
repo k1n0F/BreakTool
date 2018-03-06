c6fefc0ec267b31cc256cdc69cd7c09ec9db23e8
k1n0F <35903605+k1n0F@users.noreply.github.com>
2018-03-06 13:47:23 +0200

    Update BreakTool.py

--- BreakTool.py
+++ BreakTool.py
@@ -27,7 +27,7 @@ ver 1.0
 3.install Hydra
 4.install Ngrok
 5.install Kali Nethunter
-6.install Striker
+6.install 
 7.install WPScan
 8.install Routersploit
 9.install Weeman
@@ -42,12 +42,90 @@ ver 1.0
 18.install viSQL
 19.install Fedora
 20.install Hash-Buster
-21.install Planetwork-DDOS
+21.install Metasploit
 ___________________________
 
 """)
-  
-  loop = true
+ 
+loop = True
+
+elif what == "1":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] SQLMap installed successfully :)")
+        print("[+] Go to sqlmap folder and type 'python2 sqlmap.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+elif what == "4":
+        os.system("pkg install -y wget")
+        os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
+        os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
+        os.system("cd /data/data/com.termux/files/home && cd metasploit-framework")
+        os.system("cd /data/data/com.termux/files/home && gem install bundle --pre")
+        os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
+        os.system("cd /data/data/com.termux/files/home && bundle install")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] Metasploit installed successfully :)")
+        print("[+] Type 'msfconsole' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+elif what == "3":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y hydra")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] hydra installed successfully :)")
+        print("[+] Type 'hydra' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+elif what == "5":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] ngrok installed successfully :)")
+        print("[+] Go to ngrok folder and type './ngrok http 80' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+ elif what == "6":
+        os.system("pkg update -y")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
+        os.system("cd /data/data/com.termux/files/home && cd Nethunter-In-Termux && chmod +x kalinethunter")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] Nethunter installed successfully :)")
+        print("[+] Go to Nethunter-In-Termux folder and type './kalinethunter' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+            
 
 
 
