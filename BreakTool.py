e1d344ea0fd39384abc127bdc4224c66d560cbe6
k1n0F <35903605+k1n0F@users.noreply.github.com>
2018-03-06 17:22:45 +0200

    Update BreakTool.py

--- BreakTool.py
+++ BreakTool.py
@@ -54,9 +54,9 @@ loop = True
 while loop:
     menu()
     what = input("#: ")
-if what == "00":
+    if what == "00":
         print("================================")
-        print("This will install: nmap, sqlmap, metasploit, ngrok, angryFuzzer, red_hawk, weeman, IPGeoLocation, cupp, instahack, TwitterSniper, Hash-Buster, D-TECT, routersploit and viSQL with one click.")
+        print("This will install: nmap, hydra, sqlmap, metasploit, ngrok, angryFuzzer, red_hawk, weeman, IPGeoLocation, cupp, instahack, TwitterSniper, Hash-Buster, D-TECT, routersploit and viSQL with one click.")
         print("----------------")
         hm = input("[?] Do you want to continue? (y/n): ")
         print("================================")
@@ -163,7 +163,7 @@ if what == "00":
             if rmenu == "y":
                 menu()
             else:
-                break 
+                break
     if what == "1":
         os.system("cd /data/data/com.termux/files/home")
         os.system("pkg update -y")
@@ -177,8 +177,22 @@ if what == "00":
         if rmenu == "y":
             menu()
         else:
-            break    
-elif what == "2":
+            break
+    elif what == "2":
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg update -y")
+        os.system("pkg install -y hydra")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] hydra installed successfully :)")
+        print("[+] Type 'hydra' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "3":
         os.system("cd /data/data/com.termux/files/home")
         os.system("pkg update -y")
         os.system("pkg install -y git")
@@ -194,7 +208,7 @@ elif what == "2":
             menu()
         else:
             break
- elif what == "6":
+    elif what == "4":
         os.system("pkg install -y wget")
         os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
         os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
@@ -212,21 +226,7 @@ elif what == "2":
             menu()
         else:
             break
-elif what == "3":
-        os.system("cd /data/data/com.termux/files/home")
-        os.system("pkg update -y")
-        os.system("pkg install -y hydra")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] hydra installed successfully :)")
-        print("[+] Type 'hydra' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-elif what == "4":
+    elif what == "5":
         os.system("pkg update -y")
         os.system("pkg install -y git")
         os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
@@ -240,7 +240,7 @@ elif what == "4":
             menu()
         else:
             break
- elif what == "5":
+    elif what == "6":
         os.system("pkg update -y")
         os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
         os.system("cd /data/data/com.termux/files/home && cd Nethunter-In-Termux && chmod +x kalinethunter")
@@ -254,7 +254,177 @@ elif what == "4":
             menu()
         else:
             break
- elif what == "16":
+    elif what == "7":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git")
+        os.system("cd /data/data/com.termux/files/home && cd angryFuzzer")
+        os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
+        os.system("cd /data/data/com.termux/files/home && pip2 install requests")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] angryFuzzer installed successfully :)")
+        print("[+] Go to angryFuzzer folder and type 'python2 angryFuzzer.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "8":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y php")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] RED_HAWK installed successfully :)")
+        print("[+] Go to RED_HAWK folder and type 'php rhawk.php' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "9":
+        os.system("pkg update -y")
+        os.system("pkg install -y python2")
+        os.system("pkg install -y git")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/evait-security/weeman.git")
+        os.system("cd /data/data/com.termux/files/home && cd weeman")
+        os.system("cd /data/data/com.termux/files/home && chmod +x weeman.py")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] weeman installed successfully :)")
+        print("[+] Go to weeman folder and type 'python2 weeman.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "10":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/maldevel/IPGeoLocation.git")
+        os.system("cd /data/data/com.termux/files/home && cd IPGeoLocation")
+        os.system("cd /data/data/com.termux/files/home && pip install -r requirements.txt")
+        os.system("cd /data/data/com.termux/files/home")
+        print("====================================")
+        print("[+] IPGeoLocation installed successfully :)")
+        print("[+] Go to IPGeoLocation folder and type 'python ipgeolocation.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "11":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git")
+        print("====================================")
+        print("[+] Cupp installed successfully :)")
+        print("[+] Go to cupp folder and type 'python cupp3.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "12":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python")
+        os.system("pkg install -y nano")
+        os.system("pip install requests")
+        os.system("pip install beautifulsoup4")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git")
+        print("====================================")
+        print("[+] Instahack installed successfully :)")
+        print("[+] Go to instahack folder and type 'python hackinsta.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "13":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python")
+        os.system("pip install mechanicalsoup")
+        os.system("pkg install -y nano")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git")
+        print("====================================")
+        print("[+] TwitterSniper installed successfully :)")
+        print("[+] Go to TwitterSniper folder and type 'python TwitterSniper.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "14":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Neo-Oli/termux-ubuntu.git")
+        os.system("cd /data/data/com.termux/files/home && cd termux-ubuntu && bash ubuntu.sh")
+        print("====================================")
+        print("[+] Ubuntu installed successfully :)")
+        print("[+] Go to termux-ubuntu folder and type './start.sh' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "15":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y wget")
+        os.system("apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh")
+        print("====================================")
+        print("[+] Fedora installed successfully :)")
+        print("[+] Type 'sh termux-fedora.sh f26_arm64' or 'sh termux-fedora.sh f26_arm' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "16":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git")
+        print("====================================")
+        print("[+] viSQL installed successfully :)")
+        print("[+] Go to viSQL folder and type 'python2 viSQL.py --help' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "17":
+        os.system("pkg update -y")
+        os.system("pkg install -y git")
+        os.system("pkg install -y python2")
+        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git")
+        print("====================================")
+        print("[+] Hash-Buster installed successfully :)")
+        print("[+] Go to Hash-Buster folder and type 'python2 hash.py' to start.")
+        print("====================================")
+        rmenu = input("[?] Back to Menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "18":
         os.system("pkg update -y")
         os.system("pkg install -y git")
         os.system("pkg install -y python2")
@@ -288,7 +458,3 @@ elif what == "4":
     elif what == "99":
         print("Bye.")
         break
-            
-
-
-
