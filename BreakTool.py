520c31d8a18e2c01e4ed356d52a84b457c91637c
k1n0F <35903605+k1n0F@users.noreply.github.com>
2018-03-06 12:48:39 +0200

    Update BreakTool.py

--- BreakTool.py
+++ BreakTool.py
@@ -22,25 +22,32 @@ ver 1.0
 
 ================
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
+1.install sqlmap
+2.install Nmap
+3.install Hydra
+4.install Ngrok
+5.install Kali Nethunter
+6.install Striker
+7.install WPScan
+8.install Routersploit
+9.install Weeman
+10.install angryFuzzer
+11.install Instagram Bruteforcer
+12.install Twitter Bruteforcer
+13.install Ubuntu
+14.install Fedora
+15.install IPGeoLocation
+16.install D-TECT
+17.install Hash-Buster
+18.install viSQL
+19.install Fedora
+20.install Hash-Buster
+21.install Planetwork-DDOS
+___________________________
+
+""")
+  
+  loop = true
+
+
 
